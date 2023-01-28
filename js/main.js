dbd63f9556532d667ddd681c537cb6cab39d60d5
Nevacat <nevacattery217@naver.com>
2023-01-28 19:00:26 +0900

    Docs: Modified Mouse Cursor Effect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 const cursor = document.querySelector(".mouse__cursor");
+const cardsEl = document.querySelectorAll(".card")
 
 document.addEventListener("mousemove", function(e){
-  gsap.to(cursor, {duration: 0.2, left: e.pageX, top: e.pageY});
-});
+  gsap.to(cursor,{
+		duration: 0.3,
+		left: e.pageX-22,
+		top: e.pageY-18
+	})
+})
+
+cardsEl.forEach(card => {
+  card.addEventListener('mouseenter',()=>{
+    cursor.classList.add("active")
+    card.style.fill
+  })
+  card.addEventListener('mouseleave',()=>{
+    cursor.classList.remove("active")
+  })
+})
