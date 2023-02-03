397dd875ebb7551ac877a3d1328059d048820070
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-02-03 21:09:25 +0900

    feat: Implement slide functions

--- PyoSeungYeon/js/main.js
+++ PyoSeungYeon/js/main.js
@@ -11,3 +11,25 @@ function scrollToTabPanel() {
   const scrollAmount = tabPanel.getBoundingClientRect().top
   window.scrollBy({ top: scrollAmount - 74, behavior: 'smooth' })
 }
+
+let move = 0
+const sliderList = document.querySelector('.slider-list')
+const buttonList = document.querySelectorAll('.btn-group .btn')
+const itemCount = sliderList.childElementCount
+const movePercent = 100 / itemCount // 증감값 25
+
+buttonList.forEach((button) => {
+  button.addEventListener('click', function () {
+    if (button.classList.contains('btn-prev')) {
+      move += movePercent
+      if (move > 0) move = 0
+      sliderList.style.transform = `translateX(${move}%)`
+    }
+
+    if (button.classList.contains('btn-next')) {
+      move -= movePercent
+      if (move < -75) move = -75
+      sliderList.style.transform = `translateX(${move}%)`
+    }
+  })
+})
